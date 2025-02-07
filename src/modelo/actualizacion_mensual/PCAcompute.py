178a1c5506cb9ab6173a368fa4241cf2e9c355bb
adriangarridosaez <adrian.garrido@edu.uah.es>
2025-02-07 19:42:51 +0100

    Seguimo duro en PCA

---
+++ src/modelo/actualizacion_mensual/PCAcompute.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+from src.__special__ import indices_path
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+
+def PCAcompute():
+    pass
+
+
+if __name__ == '__main__':
+    PCAcompute()
